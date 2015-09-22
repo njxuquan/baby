40d95445382aa3cf56725e99b5929f141dabb263
njxuquan <fred_bgyb@hotmail.com>
2015-09-22 10:31:15 +0800

    test hooks

--- www.fhxckids.com/a.php
+++ www.fhxckids.com/a.php
@@ -8,24 +8,17 @@
 			}
 		}
 	} else {
-		headers = getallheaders();
+		$headers = getallheaders();
 	}
-	//github发送过来的签名
+
 	$hubSignature = $headers['X-Hub-Signature'];
 	list($algo, $hash) = explode('=', $hubSignature, 2);
-
-	// 获取body内容
 	$payload = file_get_contents('php://input');
-
-	// 计算签名
 	$payloadHash = hash_hmac($algo, $payload, $secret);
 
-	// 判断签名是否匹配
 	if ($hash === $payloadHash) {
-		   //调用shell
-		//echo exec("/data/Git.sh");
-		echo "ok";
+		echo exec("/data/Git.sh");
 	} else {
-		echo "error";
+		echo "hash error";
 	}
 ?>
